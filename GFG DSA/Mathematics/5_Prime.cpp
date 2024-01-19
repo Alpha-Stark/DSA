ba1860a99319d6b94f969ff88e4c1cad805ac052
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-19 18:51:52 +0530

    Prime Number

---
+++ GFG DSA/Mathematics/5_Prime.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkPrime(int n)
+{
+    if (n == 1)
+    {
+        return false;
+    }
+
+    int i = 2;
+    // while (i < n)        // O(n)
+    // while (i <= n / 2)   // O(n/2)
+    // while (i <= sqrt(n)) // O(√n)
+    // or
+    while (i * i <= n) // O(√n)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number:";
+    cin >> n;
+    if (checkPrime(n))
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
---
+++ GFG DSA/Mathematics/5_PrimeOpt.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkPrime(int n)
+{
+    if (n == 1)
+        return false;
+
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= n; i = i + 6)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number:";
+    cin >> n;
+    if (checkPrime(n))
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
