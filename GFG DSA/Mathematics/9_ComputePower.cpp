6938dd92e0589c103029ad0002a01ac1531e68f1
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-21 00:08:53 +0530

    ComputePower Optimized to log(n)

--- GFG DSA/Mathematics/9_ComputePower.cpp
+++ GFG DSA/Mathematics/9_ComputePower.cpp
@@ -9,13 +9,31 @@ int power(int n, int p)
         res *= n;
     }
     return res;
-}
+} // O(n)
+
+int powerOpt(int n, int p)
+{
+    if (p == 1)
+    {
+        return n;
+    }
+
+    if (p % 2 == 0)
+    {
+        return powerOpt(n, p / 2) * powerOpt(n, p / 2);
+    }
+    else
+    {
+        return powerOpt(n, p - 1) * n;
+    }
+} // O(logn)
 
 int main()
 {
     int n, p;
     cout << "Enter a number and its power:";
     cin >> n >> p;
-    cout << power(n, p) << endl;
+    // cout << power(n, p) << endl;
+    cout << powerOpt(n, p) << endl;
     return 0;
 }
