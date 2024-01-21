7a0992409136eb097b880510a4a10569bb346ec6
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-21 19:40:55 +0530

    Compute Power for larger values or return  value under a modulo

--- GFG DSA/Mathematics/9_ComputePower2.cpp
+++ GFG DSA/Mathematics/9_ComputePower2.cpp
@@ -6,7 +6,8 @@ int powerOpt(int n, int p)
     int res = 1;
     while (p > 0)
     {
-        if (p % 2 != 0)
+        // if (p % 2 != 0) //Can be also written as below, and its faster because of bitwise nature.
+        if (p & 1)
         {
             // cout << "1 ";
             res = res * n;
@@ -16,10 +17,11 @@ int powerOpt(int n, int p)
             // cout << "0 ";
         }
         n = n * n;
-        p = p / 2;
+        // p = p / 2; //Also written as below, and its faster because of bitwise nature.
+        p = p >> 1;
     }
     return res;
-} // O(logn)
+} // O(logn), better than recursive, because it's complexity is same as Recursive, but auxilary Space is O(1) only.
 
 int main()
 {
@@ -29,3 +31,30 @@ int main()
     cout << powerOpt(n, p) << endl;
     return 0;
 }
+
+/*
+
+If a number is large.
+Or told use return the value under some modulo m,
+
+long long int powerOpt(int n, int p, int m)
+{
+    long long int res = 1;
+    while (p > 0)
+    {
+        if (p & 1)
+        {
+            // cout << "1 ";
+            res = (res * n)%m;
+        }
+        else
+        {
+            // cout << "0 ";
+        }
+        n = (n * n)%m;
+        p = p >> 1;
+    }
+    return res;
+} // O(logn), better than recursive, because it's complexity is same as Recursive, but auxilary Space is O(1) only.
+
+ */
