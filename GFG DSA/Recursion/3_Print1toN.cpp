b90659f73e25ac1dd034ad12624bc0f13f1da81e
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-23 11:36:54 +0530

    Refactor printNTo1 function to optimize space complexity

--- GFG DSA/Recursion/3_Print1toN.cpp
+++ GFG DSA/Recursion/3_Print1toN.cpp
@@ -5,9 +5,9 @@ void printNTo1(int n)
 {
     if (n == 0)
         return;
-    cout << n << " ";
     printNTo1(n - 1);
-} // ø(n) and auxilary space = ø(n+1), but as it is a tail recursive function, its auxilory space will be  (1) only, due to modern compilers.(converts recursion to simple goto)
+    cout << n << " ";
+} // ø(n) and auxilary space = ø(n+1)
 
 int main()
 {
