aca1f9c8eba227e631caf8510aa502fc973b8990
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-23 11:50:14 +0530

    Converted a Recursive print1toN function to tail recursive.

--- GFG DSA/Recursion/3_Print1toN.cpp
+++ GFG DSA/Recursion/3_Print1toN.cpp
@@ -9,11 +9,31 @@ void printNTo1(int n)
     cout << n << " ";
 } // ø(n) and auxilary space = ø(n+1)
 
+// Lets change it to tail recursive
+void printNTo1Opt(int n, int k)
+{
+    if (k == n + 1)
+        return;
+    cout << k << " ";
+    printNTo1Opt(n, k + 1);
+} // Now auxilary space is O(1) only.
+
+/* GFG Code:
+
+    void fun(int n, int k){
+        if(n == 0) return;
+        cout<<n<<" ";
+        fun(n-1,k+1);
+    }
+
+ */
+
 int main()
 {
     int n;
     cout << "Enter a number: ";
     cin >> n;
-    printNTo1(n);
+    // printNTo1(n);
+    printNTo1Opt(n, 1);
     return 0;
 }
