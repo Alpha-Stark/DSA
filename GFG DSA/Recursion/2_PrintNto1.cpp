c4c785d0e56d8875c0e959a5bae423b224d0fc79
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-23 10:58:54 +0530

    Minor Changes

--- GFG DSA/Recursion/2_PrintNto1.cpp
+++ GFG DSA/Recursion/2_PrintNto1.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void nTo1(int n)
+void printNTo1(int n)
 {
     if (n == 0)
         return;
     cout << n << " ";
-    nTo1(n - 1);
+    printNTo1(n - 1);
 }
 
 int main()
 {
     int n;
     cin >> n;
-    nTo1(n);
+    printNTo1(n);
     return 0;
 }
