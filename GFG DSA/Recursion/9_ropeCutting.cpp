d88bbbfd85086f87fafe9885ffde6f277fe91a78
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-24 22:21:29 +0530

    Added Smarter way to write rope cutting problem

--- GFG DSA/Recursion/9_ropeCutting.cpp
+++ GFG DSA/Recursion/9_ropeCutting.cpp
@@ -43,3 +43,26 @@ int main()
 
     return 0;
 }
+
+/*
+
+GFG Solution:
+
+int maxCuts(int n, int a, int b, int c)
+{
+    if(n == 0)
+        return 0;
+    if(n <= -1)
+        return -1;
+
+    int res = max(maxCuts(n-a, a, b, c),
+              max(maxCuts(n-b, a, b, c),
+              maxCuts(n-c, a, b, c)));
+
+    if(res == -1)
+        return -1;
+
+    return res + 1;
+}
+
+ */
