6a6ab047cad36f517b80ff19b5c42eacbc3bfb0a
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-01-25 23:40:17 +0530

    Half way through

--- GFG DSA/Recursion/11_towerOfHanoi.cpp
+++ GFG DSA/Recursion/11_towerOfHanoi.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void towerOfHanoi(int n)
+{
+}
+
 int main()
 {
     int n;
+    cout << "Enter a number: ";
     cin >> n;
+
+    towerOfHanoi(n);
 
     return 0;
 }
