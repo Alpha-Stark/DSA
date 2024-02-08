68e1a8690fb77964b90036e509a01b170bd7c2a3
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-08 11:53:31 +0530

    Array: 2ndLargestElement Correct Code

--- GFG DSA/Array/3_2ndLargestElement.cpp
+++ GFG DSA/Array/3_2ndLargestElement.cpp
@@ -3,31 +3,25 @@ using namespace std;
 
 int secondLargestElement(int arr[], int n)
 {
-    if (n == 0 || n == 1)
-        return -1;
+    int secondMax = -1;
+    int largest = 0;
 
-    int maxIndex = 0;
-    int secondMaxIndex = 1;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        if (arr[i] > arr[maxIndex])
+        if (arr[i] > arr[largest])
         {
-            maxIndex = i;
+            secondMax = largest;
+            largest = i;
         }
-
-        if (arr[i] < arr[maxIndex] && arr[i] > arr[secondMaxIndex])
+        else if (arr[i] != arr[largest])
         {
-            secondMaxIndex = i;
+            if (secondMax == -1 || arr[i] > arr[secondMax])
+            {
+                secondMax = i;
+            }
         }
     }
-    if (arr[maxIndex] == arr[secondMaxIndex])
-    {
-        return -1;
-    }
-    else
-    {
-        return secondMaxIndex;
-    }
+    return secondMax;
 }
 
 int main()
