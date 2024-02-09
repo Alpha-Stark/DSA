9a5a312b1c0e093db9ed4b605b4402bea99ec4b1
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-09 16:28:49 +0530

    Array: Remove Dublicates in Sorted Array Optimized solution. O(n)

--- GFG DSA/Array/7_removeDublicatesSorted.cpp
+++ GFG DSA/Array/7_removeDublicatesSorted.cpp
@@ -23,6 +23,22 @@ int removeDublicates(int *arr, int n)
     return res;
 } // O(n^2)
 
+int removeDublicates2(int *arr, int n)
+{
+    int i = 1;
+    int j = 0;
+    for (i = 1; i < n; i++)
+    {
+        if (arr[j] != arr[i])
+        {
+            arr[j + 1] = arr[i];
+            j++;
+            /* int temp = arr[j + 1]; arr[j + 1] = arr[i]; arr[i] = temp; // instead of swapping we, can just assign the arr[i] to the jth index, because that swapped or unswapped element wont be touched after that.*/
+        }
+    }
+    return j + 1; // because simple j is index, but we have to send the number of elements, so total number of element will be j+1 in the array;
+} // O(n)
+
 int main()
 {
     int n;
@@ -33,7 +49,7 @@ int main()
         cin >> arr[i];
     }
 
-    int newN = removeDublicates(arr, n);
+    int newN = removeDublicates2(arr, n);
 
     for (int i = 0; i < newN; i++)
     {
