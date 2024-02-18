111783236229a8ee85e3d0ec2cadf43b89442e3f
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-18 16:33:34 +0530

    Array: Trapping Rain Water Optimized

--- GFG DSA/Array/14_trappingRainWater.cpp
+++ GFG DSA/Array/14_trappingRainWater.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int trappedWater(int *arr, int n)
+{
+    /* Now how do we get here? from the below solution, there we understood that every element need its left and right max, without it, it was not possible to solve this problem.
+       And we didn't wanted the n^2 complexity, so the only way is to keep pre-calculate the left max and right max for all elements using extra spaces, and based on those we solve the problem.
+    */
+    int leftMax[n]; // left max array
+    leftMax[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        leftMax[i] = max(arr[i], leftMax[i - 1]); // By using last element's value we can get left max and no need maintain currentMax variable.
+    }
+
+    int rightMax[n]; // right max array
+    rightMax[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightMax[i] = max(arr[i], rightMax[i + 1]);
+    }
+
+    int res = 0;
+    for (int i = 1; i <= n - 2; i++)
+    {
+        res += min(leftMax[i], rightMax[i]) - arr[i];
+    }
+
+    return res;
+}
+
 int trappedWater2(int *arr, int n)
 {
     int res = 0;
@@ -20,7 +48,7 @@ int trappedWater2(int *arr, int n)
     }
 
     return res;
-}
+} // ø(n^2)
 
 int main()
 {
@@ -31,7 +59,7 @@ int main()
     {
         cin >> arr[i];
     }
-    cout << trappedWater2(arr, n) << endl;
+    cout << trappedWater(arr, n) << endl;
 
     return 0;
 }
