bb54dbea517ec6dd070b2be013dc141b743ae426
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-27 23:23:30 +0530

    Searching: Binary Search

---
+++ GFG DSA/Searching/1_binarySearch.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (x < arr[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n, x;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> x;
+    cout << binarySearch(arr, n, x) << endl;
+
+    return 0;
+}
