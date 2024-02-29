d1ceb31761b1b71deeccb496e4cf36e25138a2ed
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-02-29 11:05:19 +0530

    Searching in Rotated Sorted array (My solution)

---
+++ GFG DSA/Searching/3_sortedRotated.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sortedRotated(int arr[], int low, int high, int x)
+{
+    int mid = (low + high) / 2;
+    if (arr[mid] == x)
+        return mid;
+
+    if (low >= high)
+    {
+        return -1;
+    }
+
+    if (arr[low] < arr[mid])
+    {
+        if (arr[low] <= x && x <= arr[mid - 1])
+            return sortedRotated(arr, low, mid - 1, x);
+        else
+            return sortedRotated(arr, mid + 1, high, x);
+    }
+    else
+    {
+        if (arr[mid + 1] <= x && x <= arr[high])
+            return sortedRotated(arr, mid + 1, high, x);
+        else
+            return sortedRotated(arr, low, mid - 1, x);
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int x;
+    cin >> x;
+    cout << sortedRotated(arr, 0, n - 1, x) << endl;
+
+    return 0;
+}
+
+/*
+
+7
+10 20 30 40 50 8 9
+30
+
+***
+5
+100 200 300 10 20
+40
+
+7
+100 200 300 400 20 30 40
+50
+
+7
+100 500 10 20 30 40 50
+40 or 500 or 100
+
+7
+100 200 500 1000 2000 10 20
+25
+
+*/
