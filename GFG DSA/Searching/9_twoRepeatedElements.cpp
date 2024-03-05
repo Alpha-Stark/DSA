96d535f6765c8717c476423d65c7597860203f54
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-03-05 10:19:05 +0530

    Searching: 2 repeated elements (Imp Learning Algorithm)

--- GFG DSA/Searching/9_twoRepeatedElements.cpp
+++ GFG DSA/Searching/9_twoRepeatedElements.cpp
@@ -14,71 +14,22 @@ public:
     vector<int> twoRepeated(int arr[], int n)
     {
         vector<int> ans;
-        int count = 0;
-        int element1 = -1;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n + 2; i++)
         {
-            if (count == 0)
+            int toCheck = arr[abs(arr[i])];
+            if (toCheck >= 0)
             {
-                element1 = arr[i];
-                count = 1;
+                arr[abs(arr[i])] = -arr[abs(arr[i])];
             }
             else
             {
-                if (arr[i] == element1)
-                {
-                    count++;
-                }
-                else
-                {
-                    count--;
-                }
+                // print(abs(arr[i]));
+                ans.push_back(abs(arr[i]));
             }
         }
-
-        int element2 = -1;
-        count = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] == element1)
-                continue;
-
-            if (count == 0)
-            {
-                element2 = arr[i];
-                count = 1;
-            }
-            else
-            {
-                if (arr[i] == element2)
-                {
-                    count++;
-                }
-                else
-                {
-                    count--;
-                }
-            }
-        }
-        cout << "Element1: " << element1 << " Element2: " << element2 << endl;
-        int maxIndex1 = 0, maxIndex2 = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] == element1)
-            {
-                maxIndex1 = max(maxIndex1, i);
-            }
-            else if (arr[i] == element2)
-            {
-                maxIndex2 = max(maxIndex2, i);
-            }
-        }
-        ans.push_back(arr[min(maxIndex1, maxIndex2)]);
-        ans.push_back(arr[max(maxIndex1, maxIndex2)]);
         return ans;
     }
 };
-
 
 int main()
 {
