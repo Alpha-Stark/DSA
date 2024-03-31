3f547204bb088c8f2821d4c47c36c1c48e5e4a15
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-03-31 16:41:32 +0530

    Matrix: Snake Pattern

--- GFG DSA/Matrix/1_snakePattern.cpp
+++ GFG DSA/Matrix/1_snakePattern.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 // To be allowed to declare a multidementional vector, have to change formatting setting, as it wont be done just by disabelling prettier extension. In settings, under format section, changed the "c_cpp: Formatting" from "default" to "vsFormat". 
 
+void snakePattern(const vector<vector<int> >& A) {
+    int r = A.size(), c = A[0].size();
+    for (int i = 0; i < r; i++)
+    {
+        if (i % 2 == 0) {
+            for (int j = 0; j < c; j++)
+                cout << A[i][j] << " ";
+        }
+        else {
+            for (int j = c - 1; j >= 0; j--)
+                cout << A[i][j] << " ";
+        }
+    }
+}
+
 int main()
 {
     int r, c;
@@ -15,15 +30,11 @@ int main()
             cin >> A[i][j];
         }
     }
-
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cout << A[i][j] << " ";
-        }
-        cout << endl;
-    }
-
+    snakePattern(A);
     return 0;
 }
+
+/*
+3 3
+45 48 54 21 89 87 70 78 15
+*/
