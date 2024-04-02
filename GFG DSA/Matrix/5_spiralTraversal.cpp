f9157162d897bd25f23906814c0c90b190a8ba20
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-04-02 14:55:46 +0530

    Matrix: Spiral Traversal (Bug free, Updated)

--- GFG DSA/Matrix/5_spiralTraversal.cpp
+++ GFG DSA/Matrix/5_spiralTraversal.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 // To be allowed to declare a multidementional vector, have to change formatting setting, as it wont be done just by disabelling prettier extension. In settings, under format section, changed the "c_cpp: Formatting" from "default" to "vsFormat". 
 
-
+// Sir's code
 void spiralTraversal(vector<vector<int> >& A) {
     int r = A.size(), c = A[0].size();
     if (r == 1)
@@ -16,34 +16,34 @@ void spiralTraversal(vector<vector<int> >& A) {
     }
     else {
         int top = 0, right = c - 1, bottom = r - 1, left = 0;
-        /*
-        top row
-        right column
-        bottom row (reverse traversal)
-        left column (reverse traversal)
-        */
-        while (top <= bottom && left <= right) //if we put || between the conditions too, it works fine.
-        {
-            for (int j = left; j <= right; j++)
-                cout << A[top][j] << " ";
+        while (top <= bottom && left <= right) {
+            //  Print the first row from the remaining rows
+            for (int i = left; i <= right; i++)
+                cout << A[top][i] << " ";
             top++;
 
+            //  Print the last column from the remaining columns
             for (int i = top; i <= bottom; i++)
                 cout << A[i][right] << " ";
             right--;
 
-            for (int j = right;j >= left;j--)
-                cout << A[bottom][j] << " ";
-            bottom--;
+            // Print the last row from the remaining rows
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    cout << A[bottom][i] << " ";
+                bottom--;
+            }
 
-            for (int i = bottom; i >= top; i--)
-                cout << A[i][left] << " ";
-            left++;
+            // Print the first column from the remaining columns
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    cout << A[i][left] << " ";
+                left++;
+            }
         }
-
     }
-
 }
+
 
 void printMatrix(const vector<vector<int> >& A) {
     cout << endl;
@@ -101,44 +101,43 @@ int main()
 */
 
 
-// Sir's code:
+// My code, this prints dublicates at the end
 /*
+void spiralTraversal(vector<vector<int> >& A) {
+    int r = A.size(), c = A[0].size();
+    if (r == 1)
+    {
+        for (int j = 0;j < c;j++)
+            cout << A[0][j] << " ";
+    }
+    else if (c == 1) {
+        for (int i = 0;i < r;i++)
+            cout << A[i][0] << " ";
+    }
+    else {
+        int top = 0, right = c - 1, bottom = r - 1, left = 0;
 
-void spiralPrint(int m, int n, int a[R][C])
-{
-    int top = 0, left = 0, bottom=r-1, right= c-1;
+        // top row
+        // right column
+        // bottom row (reverse traversal)
+        // left column (reverse traversal)
 
-        // top - starting row index (k)
-        // right - ending row index (m)
-        // bottom - starting column index (l)
-        // left - ending column index (n)
-        // i - iterator
+        while (top <= bottom && left <= right) //if we put || between the conditions too, it works fine.
+        {
+            for (int j = left; j <= right; j++)
+                cout << A[top][j] << " ";
+            top++;
 
-    while (top <= bottom && left <= right) {
-        //  Print the first row from the remaining rows
-        for (int i = left; i <= right; i++) {
-            cout << a[top][i] << " ";
-        }
-        top++;
-        //  Print the last column from the remaining columns
-        for (int i = top; i <= bottom; i++) {
-            cout << a[i][right] << " ";
-        }
-        right--;
+            for (int i = top; i <= bottom; i++)
+                cout << A[i][right] << " ";
+            right--;
 
-        // Print the last row from the remaining rows
-        if (top <= bottom) {
-            for (int i = right; i >= left; i--) {
-                cout << a[bottom][i] << " ";
-            }
+            for (int j = right;j >= left;j--)
+                cout << A[bottom][j] << " ";
             bottom--;
-        }
 
-        // Print the first column from the remaining columns
-        if (left <= right) {
-            for (int i = bottom; i >= top; i--) {
-                cout << a[i][left] << " ";
-            }
+            for (int i = bottom; i >= top; i--)
+                cout << A[i][left] << " ";
             left++;
         }
     }
