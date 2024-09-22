03c11f55973ef6549623e9912b12991d7eadbc60
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-09-22 18:53:49 -0400

    Added Comments for before question.

--- GFG DSA/Strings/5_ReverseWordsInAString.cpp
+++ GFG DSA/Strings/5_ReverseWordsInAString.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+/*
+So there are two steps of solving this problem.
+That is,
+1. 1st we reverse the full string. And then,
+2. We reverse the individual words.
+So at the end we get the desired full string.
+
+We have seen this kind of question before, so pattern should be recognized in yourm mind and that's how it is solved.
+Sir, in the video follow similar but slightly different approach.
+ */
+
 void reverse(string& s, int startI, int endI) {
     int j = endI;
     for (int i = startI; i <= (startI + endI) / 2; i++) {
